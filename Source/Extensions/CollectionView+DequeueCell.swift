995133d66c7f3d0feb03aed982c3d17c817bb41c
PM-Git <50369307+pm-git@users.noreply.github.com>
2020-09-07 12:28:17 +0530

    changed access modifire to public for collectiob view extensions

--- Source/Extensions/CollectionView+DequeueCell.swift
+++ Source/Extensions/CollectionView+DequeueCell.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-extension UICollectionView {
+public extension UICollectionView {
 
     func dequeue<T: UICollectionViewCell> (_: T.Type,
                                            for indexPath: IndexPath) -> T {
